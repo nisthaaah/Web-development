0670b79d3241504dcf82a903de50dc64344b5287
nistha <nisthasharma0799@gmail.com>
2024-01-21 00:30:14 +0530

    currency convertor

--- Currency Convertor/index.js
+++ Currency Convertor/index.js
@@ -7,43 +7,42 @@ const toCurr = document.querySelector(".to select");
 const msg = document.querySelector(".msg")
 
 
-for(let select of dropdowns){
-    for(currCode in countryList){
+for (let select of dropdowns) {
+    for (currCode in countryList) {
         let newOption = document.createElement("option");
         newOption.innerText = currCode;
         newOption.value = currCode;
-        if(select.name === "from" && currCode === "USD"){
+        if (select.name === "from" && currCode === "USD") {
             newOption.selected = "selected";
         }
-        else if(select.name === "to" && currCode === "INR"){
+        else if (select.name === "to" && currCode === "INR") {
             newOption.selected = "selected";
         }
         select.append(newOption);
 
-        select.addEventListener("change", (e)=>{
+        select.addEventListener("change", (e) => {
             updateFlag(e.target);
         });
     }
 }
 
-const updateExchange = async() => {
+const updateExchange = async () => {
     let amount = document.querySelector(".amount input");
     let amtval = amount.value;
-    if(amtval == "" || amtval < 1){
+    if (amtval == "" || amtval < 1) {
         amtval = 1;
         amount.value = "1";
-}
+    }
 
- //console.log(fromCurr.value, toCurr.value);
- const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
- let response = await fetch(URL);
- let data = await response.json();
- let rate = data[toCurr.value.toLowerCase()];
- 
- let res = amtval * rate;
+    //console.log(fromCurr.value, toCurr.value);
+    const URL = `${BASE_URL}/${fromCurr.value.toLowerCase()}/${toCurr.value.toLowerCase()}.json`;
+    let response = await fetch(URL);
+    let data = await response.json();
+    let rate = data[toCurr.value.toLowerCase()];
 
- msg.innerText = `${amtval} ${fromCurr.value} = ${res} ${toCurr.value}`;
+    let res = amtval * rate;
 
+    msg.innerText = `${amtval} ${fromCurr.value} = ${res} ${toCurr.value}`;
 };
 
 const updateFlag = (ele) => {
@@ -54,11 +53,11 @@ const updateFlag = (ele) => {
     img.src = newSrc;
 }
 
-btn.addEventListener("click", async (e) =>{
+btn.addEventListener("click", async (e) => {
     e.preventDefault();
     updateExchange();
-    });
+});
 
-window.addEventListener("load", ()=>{
+window.addEventListener("load", () => {
     updateExchange();
 });
